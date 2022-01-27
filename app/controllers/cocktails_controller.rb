dd5d5f5dffb5212b6b4ad20157c5c7fd9fad3fce
Jacqui <jt.cope1@gmail.com>
2022-01-27 12:34:37 +0800

    adds all actions and functions

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,26 +2,42 @@ require 'json'
 class CocktailsController < ApplicationController
     before_action :load_cocktails
     skip_before_action :verify_authenticity_token
+    before_action :set_cocktail, only: [:show, :update, :destroy, :edit]
 
     def index
-        render json: @cocktails
+        # render json: @cocktails
+    end
+
+    def new
+
+    end
+
+    def edit
+    
     end
 
     def create
         new_cocktail = {id: @cocktails.last["id"] + 1,  name: params[:name], base: params[:base], instructions: params[:instructions]} 
         @cocktails.push(new_cocktail)
         save_cocktails(@cocktails)
-        render json: new_cocktail, status: 201
+        redirect_to cocktails_path
     end 
 
     def show
-        cocktail = @cocktails.find {|cocktail| cocktail["id"] == params[:id].to_i}
-        # pp params[:id] #can use a symbol or string. Params always come back as a string
-        if cocktail
-            render json: cocktail
-        else
-            render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
-        end
+        # render json: @cocktail
+    end
+
+    def update
+        new_cocktail = {id: @cocktail["id"], name: params[:name], base: params[:base], instructions: params[:instructions]}
+        @cocktails[@index] = new_cocktail
+        save_cocktails(@cocktails)
+        redirect_to cocktail_path(new_cocktail[:id])
+    end
+
+    def destroy
+        @cocktails.delete_at(@index)
+        save_cocktails(@cocktails)
+        redirect_to cocktails_path #sends a get request to "get '/cocktails', to: 'cocktails#index', as: 'cocktails'"
     end
 
     private
@@ -30,8 +46,17 @@ class CocktailsController < ApplicationController
         @cocktails = JSON.parse(File.read(Rails.public_path.join('cocktails.json')))
        end 
     
-       def save_cocktails(cocktails)
+    def save_cocktails(cocktails)
         File.write(Rails.public_path.join('cocktails.json'), JSON.generate(cocktails))
-       end 
+    end 
+
+    def set_cocktail
+        @cocktail = @cocktails.find {|cocktail| cocktail["id"] == params[:id].to_i}
+        unless @cocktail
+            render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
+            return
+        end
+        @index = @cocktails.index {|cocktail| cocktail["id"] == @cocktail["id"]}
+    end
       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
   get '/cocktails', to: 'cocktails#index', as: 'cocktails'
-  post '/cocktails', to: 'cocktails#create'
+  post '/cocktails', to: 'cocktails#create', as: 'create_cocktail'
+  get '/cocktails/new', to: 'cocktails#new', as: 'new_cocktail' #Two things will happen. Will render a form with a submit button
   get '/cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+  put '/cocktails/:id', to: 'cocktails#update', as: 'update_cocktail'
+  patch '/cocktails/:id', to: 'cocktails#update'
+  delete '/cocktails/:id', to: 'cocktails#destroy'
+  get '/cocktails/:id/edit', to: 'cocktails#edit', as: 'edit_cocktail'
 end
