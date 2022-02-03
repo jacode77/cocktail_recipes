d573ad5e2d3af07ae8e3e24150073d5f57e98781
Jacqui <jt.cope1@gmail.com>
2022-02-03 09:45:02 +0800

    adds more styling

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,11 +1,31 @@
 module CocktailsHelper
-    # now no longer needed as the default is updated in the en.yml file
-    # def generate_button_text(cocktail)
-    #     if cocktail.id
-    #         return "Update #{cocktail.name}"
-    #     else
-    #         return "Create something great!"
-    #     end
-    # end
-    
+    def generate_button_text(cocktail)
+        if cocktail.id
+            return "Update #{cocktail.name}"
+        else
+            return "Create something great!"
+        end
+    end
+
+    def select_border_color(id)
+        case id
+        when 1
+            return "border-top: 10px solid #935678"
+        when 2
+            return "border-top: 10px solid #abc472"
+        when 3
+            return "border-top: 10px solid #9D702E"
+        when 4
+            return "border-top: 10px solid #d29062"
+        when 5
+            return "border-top: 10px solid #ffb600"
+        when 6
+            return "border-top: 10px solid #B7621B"
+        when 7
+            return "border-top: 10px solid #d7d9d0"
+        else
+            return "border-top: 10px solid #000000"
+        end
+    end
 end
+# "Vodka", "Gin", "Rum", "Whisky", "Bourbon", "Brandy", "Vermouth", "Moonshine"
